3b7038ccdd235e9d7f1c3535fd71bbfee46de478
mlj <7284232+utuana@users.noreply.github.com>
2024-08-25 08:46:28 -0500

    stow

--- lua/options.lua
+++ lua/options.lua
@@ -62,4 +62,6 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,resize,options,globals"
+
 -- vim: ts=2 sts=2 sw=2 et
