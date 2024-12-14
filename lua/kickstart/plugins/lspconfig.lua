eb04c6ed7dd9105cac4d161c19cfbfe721fe9380
mlj <7284232+utuana@users.noreply.github.com>
2024-12-14 11:00:29 -0600

    lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -191,6 +191,12 @@ return {
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
+              workspace = {
+                -- Add WezTerm config folder to Lua's workspace for LSP awareness
+                library = {
+                  vim.fn.expand '~/.config/wezterm',
+                },
+              },
             },
           },
         },
