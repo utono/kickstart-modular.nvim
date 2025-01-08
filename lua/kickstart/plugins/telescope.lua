28f86c393c9f26143068be6a0a2d4686644eefea
mlj <7284232+utuana@users.noreply.github.com>
2025-01-08 04:48:51 -0600

    md

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -75,7 +75,7 @@ return {
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sp', builtin.help_tags, { desc = '[S]earch Hel[p]' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
@@ -110,7 +110,7 @@ return {
       end, { desc = '[S]earch [N]eovim files' })
 
       -- Keymap for searching .md and .rst files in ~/utono/md-arch
-      vim.keymap.set('n', '<leader>sm', function()
+      vim.keymap.set('n', '<leader>sh', function()
         builtin.live_grep {
           cwd = '~/utono/md-arch',
           glob_pattern = { '*.md', '*.rst' },
@@ -121,7 +121,7 @@ return {
             preview_width = 0.6, -- Make the preview take 60% of the window width
           },
         }
-      end, { desc = '[S]earch [M]arkdown and RST files' })
+      end, { desc = 'Search Markdown and RST files' })
     end,
   },
 }
