3e3f15abf4b6ecbf7883c65f8fa386465d606a6c
mlj <7284232+utuana@users.noreply.github.com>
2025-02-14 19:28:11 -0600

    neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,17 +11,23 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-    -- { ',', ':Neotree focus<CR>', desc = 'NeoTree focus (comma)', silent = true },
+    { '\\', ':Neotree reveal_force_cwd<CR>', desc = 'NeoTree reveal with forced cwd', silent = true },
+    { '+', ':Neotree source=buffers position=right toggle<CR>', desc = 'Toggle NeoTree Buffers Right', silent = true },
+    { 'q', ':Neotree close<CR>', desc = 'Close NeoTree window', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          -- [','] = 'close_window', -- Optional: Map comma to close Neo-tree windows within the plugin
           ['\\'] = 'close_window', -- Optional: Map comma to close Neo-tree windows within the plugin
         },
       },
     },
   },
 }
+
+-- nnoremap / :Neotree toggle current reveal_force_cwd<cr>
+-- nnoremap | :Neotree reveal<cr>
+-- nnoremap gd :Neotree float reveal_file=<cfile> reveal_force_cwd<cr>
+-- nnoremap <leader>b :Neotree toggle show buffers right<cr>
+-- nnoremap <leader>s :Neotree float git_status<cr>
